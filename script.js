27fea5324cb12fbaa38e88660930f34f00503936
Misha Grekk <grekkmisha@gmail.com>
2022-09-30 14:14:55 +0300

    Отправил второй урок

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
-
-alert("Bla");
-console.log("Bla-bla");
+let title = "Yoga skunk";
+console.log(typeof title);
+let screens = '("Простые, Сложные, Интерактивные")';
+console.log(screens.length);
+console.log(screens.toLowerCase());
+console.log(screens.split(", "));
+let screenPrice = "Стоимость вёрскти экраснов " + 15000 + " рублей";
+console.log(screenPrice);
+let rollback = 10;
+console.log("Откат посреднику за работу " + 10000 * (10 / 100) + " рублей");
+let fullPrice = "Стоимость разработки сайта " + 100000 + " рублей";
+console.log(typeof fullPrice);
+console.log(fullPrice);
+let adaptive = true;
+console.log(typeof adaptive);
